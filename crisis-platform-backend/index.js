e5b7102aa18fdfd219afc189abd8dce3a6ab1b5e
vrinda0512 <vrinda0506@gmail.com>
2025-12-25 20:30:34 +0530

    frontend+backend's step 1

---
+++ crisis-platform-backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
