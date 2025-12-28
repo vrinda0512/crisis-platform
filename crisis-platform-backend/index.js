1af89dd4549e2956869cfc5c42d2f0a8593a298b
vrinda0512 <vrinda0506@gmail.com>
2025-12-28 22:53:33 +0530

    Backend doen till  step 4

--- crisis-platform-backend/index.js
+++ crisis-platform-backend/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 // Test route
 app.get("/", (req, res) => {
---
+++ crisis-platform-backend/models/Incident.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const IncidentSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+      enum: ["Medical", "Fire", "Flood", "Other"],
+    },
+
+    severity: {
+      type: String,
+      required: true,
+      enum: ["Low", "Medium", "High"],
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "Reported",
+      enum: ["Reported", "Assigned", "Resolved"],
+    },
+  },
+  {
+    timestamps: true, // adds createdAt & updatedAt automatically
+  }
+);
+
+module.exports = mongoose.model("Incident", IncidentSchema);
